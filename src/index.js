fec3793a1888ec01d3493eb205c77734eaa78a10
bookstore <booookstore@icloud.com>
2018-07-27 20:20:15 +0900

    feat : Chaiを使ってテストコードを作成

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var inversify_config_1 = require("./inversify.config");
 var types_1 = require("./types");
+var chai_1 = require("chai");
 var warrior = inversify_config_1.container.get(types_1.TYPES.Warrior);
-console.log(warrior.fight());
-console.log(warrior.snake());
+chai_1.expect(warrior.fight()).to.be.eql("cut!");
+chai_1.expect(warrior.snake()).to.be.eql("hit!");
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import {container} from "./inversify.config";
 import {TYPES} from "./types";
 import {Warrior} from "./interfaces";
 
+import {expect} from "chai";
+
 const warrior = container.get<Warrior>(TYPES.Warrior);
 
-console.log(warrior.fight());
-console.log(warrior.snake());
+expect(warrior.fight()).to.be.eql("cut!")
+expect(warrior.snake()).to.be.eql("hit!")
