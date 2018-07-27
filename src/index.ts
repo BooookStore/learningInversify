5e227baa6e2ccc95428a70c2c9c3fec347e7084a
bookstore <booookstore@icloud.com>
2018-07-27 20:41:43 +0900

    feat : コンテナのモジュール機能を使用

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,12 @@ import {TYPES} from "./types";
 import {Warrior} from "./interfaces";
 
 import {expect} from "chai";
+import { Ninja } from "./entities";
 
 const warrior = container.get<Warrior>(TYPES.Warrior);
+const ninja = container.get<Ninja>("Ninja");
 
-expect(warrior.fight()).to.be.eql("cut!")
-expect(warrior.snake()).to.be.eql("hit!")
+expect(warrior.fight()).to.be.eql("cut!");
+expect(warrior.snake()).to.be.eql("hit!");
+
+expect(ninja.fight()).to.be.eql("cut!");
--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -1,11 +1,19 @@
-import {Container} from "inversify";
+import {Container, ContainerModule, interfaces} from "inversify";
 import {TYPES} from "./types";
 import {Warrior, Weapon, ThrowableWeapon} from "./interfaces";
 import {Ninja, Katana, Shuriken} from "./entities";
 
+// Normal Container
 const container = new Container();
 container.bind<Warrior>(TYPES.Warrior).to(Ninja);
 container.bind<Weapon>(TYPES.Weapon).to(Katana);
 container.bind<ThrowableWeapon>(TYPES.ThorwableWeapon).to(Shuriken);
+
+// Container Module
+let warriors = new ContainerModule((bind: interfaces.Bind, unbind: interfaces.Unbind) => {
+    bind<Ninja>("Ninja").to(Ninja);
+})
+
+container.load(warriors);
 
 export {container}
